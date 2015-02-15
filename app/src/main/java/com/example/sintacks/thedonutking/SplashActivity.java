35ac334af28f1371ecd34a488991f60a6f36fbe0
Wayne <kingwayne79@gmail.com>
2015-02-15 15:09:42 -0500

    first commit after setup

--- app/src/main/java/com/example/sintacks/thedonutking/SplashActivity.java
+++ app/src/main/java/com/example/sintacks/thedonutking/SplashActivity.java
@@ -1,11 +1,14 @@
 package com.example.sintacks.thedonutking;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ImageView;
 
+import com.easyandroidanimations.library.Animation;
+import com.easyandroidanimations.library.AnimationListener;
 import com.easyandroidanimations.library.FadeInAnimation;
 
 
@@ -19,10 +22,18 @@ public class SplashActivity extends ActionBarActivity {
         setContentView(R.layout.activity_splash);
         getSupportActionBar().hide();
 
+        final Intent mainIntent = new Intent(this, MainActivity.class);
+
         splashIV = (ImageView)findViewById(R.id.splash_image_view);
 
         new FadeInAnimation(splashIV)
                 .setDuration(4000)
+                .setListener(new AnimationListener() {
+                    @Override
+                    public void onAnimationEnd(Animation animation) {
+                        startActivity(mainIntent);
+                    }
+                })
                 .animate();
     }
 
