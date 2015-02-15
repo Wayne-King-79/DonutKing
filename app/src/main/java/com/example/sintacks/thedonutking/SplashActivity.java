92f1196c82874fc1be8b2cfc31ee10793d9a6744
Wayne <kingwayne79@gmail.com>
2015-02-15 17:36:12 -0500

    blah

--- app/src/main/java/com/example/sintacks/thedonutking/SplashActivity.java
+++ app/src/main/java/com/example/sintacks/thedonutking/SplashActivity.java
@@ -1,6 +1,7 @@
 package com.example.sintacks.thedonutking;
 
 import android.content.Intent;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -22,7 +23,7 @@ public class SplashActivity extends ActionBarActivity {
         setContentView(R.layout.activity_splash);
         getSupportActionBar().hide();
 
-        final Intent mainIntent = new Intent(this, MainActivity.class);
+        //final Intent mainIntent = new Intent(this, MainActivity.class);
 
         splashIV = (ImageView)findViewById(R.id.splash_image_view);
 
@@ -31,33 +32,9 @@ public class SplashActivity extends ActionBarActivity {
                 .setListener(new AnimationListener() {
                     @Override
                     public void onAnimationEnd(Animation animation) {
-                        startActivity(mainIntent);
+                        //startActivity(mainIntent);
                     }
                 })
                 .animate();
     }
-
-/*
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_splash, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-    */
 }
